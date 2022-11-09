5dc802b796771e4c02a07506b7ce597c6ee86994
rodriggj <gabriel.rodriguez.ctr@gmail.com>
2022-11-09 13:51:20 -0700

    adding v3

---
+++ Demo/ELK/v3/main.tf
@@ -0,0 +1,101 @@
+# module "ec2_instance" {
+#     source = "terraform-aws-modules/ec2-instance/aws"
+#     version = "3.0"
+#     name = "elk-demo"
+#     ami = "ami-0d593311db5abb72b"
+#     key_name = aws_key_pair.elk-key.key_name
+#     instance_type = "m4.large"
+#     vpc_security_group_ids = [aws_security_group.allow_elk.id]
+#     depends_on = [
+#       aws_security_group.allow_elk
+#     ]
+
+#     tags = {
+#         terraform = true
+#         environment = "dev"
+#     }
+# }
+
+resource "aws_instance" "ec2_example" {
+
+    ami = "ami-0d593311db5abb72b"  
+    instance_type = "m4.large" 
+    key_name= "elk-key"
+    vpc_security_group_ids = [aws_security_group.allow_elk.id]
+    depends_on = [
+      aws_security_group.allow_elk
+    ]
+
+    tags = {
+        terraform = true
+        environment = "dev"
+    }
+
+  provisioner "remote-exec" {
+    inline = [
+      "touch hello.txt",
+      "echo helloworld remote provisioner >> hello.txt",
+    ]
+  }
+  connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = "ubuntu"
+      private_key = file("${path.module}/elk-key")
+      timeout     = "4m"
+   }
+}
+
+resource "aws_key_pair" "elk-key"{
+    key_name = "elk-key"
+    public_key = file("${path.module}/elk-key.pub")
+}
+
+resource "aws_security_group" "allow_elk" {
+    name        = "allow_elk"
+    description = "Allow all elasticsearch traffic"
+
+    # elastisearch port
+    ingress {
+        from_port        = 9200
+        to_port          = 9200
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+    #kibana port
+    ingress {
+        from_port        = 5601
+        to_port          = 5601
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+    #logstash port
+    ingress {
+        from_port        = 5043
+        to_port          = 5044
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+    #ssh port
+    ingress {
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    tags = {
+        Name = "allow_tls"
+    }
+}
